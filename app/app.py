9a8e61b9c00eaeb830d2422dcb6ed7696979b502
k7vinilstorage <jabtorezan@gmail.com>
2026-04-13 12:12:47 -0300

    created basic landing page html, css and added meninas logo. also render landing page in flask app

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello, Flask!</h1><p>Your app is running.</p>"
+    return render_template("landing_page.html")
 
 @app.route("/ping")
 def ping():
