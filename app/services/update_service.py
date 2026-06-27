919904692d70d255a4f4c90c699f1b633d332a42
arthur-hj <arthurhenriquejcp@gmail.com>
2026-06-27 16:11:29 -0300

    update "finalizado"

--- app/services/update_service.py
+++ app/services/update_service.py
@@ -12,10 +12,6 @@ def update_user_from_user(data):
     if not (data.get("Email")or"").email_validate():
         return {"success": False, "message": "Email inválido."}, 400
     
-    # validar rg
-    if not (data.get("RG")or"").rg_validate(rg):
-        return {"success": False, "message": "RG inválido."}, 400
-    
     # validar profissão
     if not (data.get("Profissao")or"").replace(" ","").isalpha() or len((data.get("Profissao")or"")) > 100:
         return {"success": False, "message": "A profissão deve conter apenas letras."}, 400
@@ -33,62 +29,57 @@ def update_user_from_user(data):
         return {"success": False, "message": "O RA deve conter apenas números e ser menor que 20 caracteres."}, 400
     
     #validar rua
-    if not street.replace(" ","").isalpha() or len(street) > 150:
+    if not (data.get("Logradouro")or"").replace(" ","").isalpha() or len((data.get("Logradouro")or"")) > 150:
         return {"success": False, "message": "A rua deve conter apenas letras e ser menor que 150 caracteres."}, 400
     
     #validar bairro
-    if not neighborhood.replace(" ","").isalpha() or len(neighborhood) > 100:
+    if not (data.get("Bairro")or"").replace(" ","").isalpha() or len((data.get("Bairro")or"")) > 100:
         return {"success": False, "message": "O bairro deve conter apenas letras e ser menor que 100 caracteres."}, 400
     
     #validar cep
-    if not postal_code.isdigit() or len(postal_code) != 8:
+    if not (data.get("CEP")or"").isdigit() or len((data.get("CEP")or"")) != 8:
         return {"success": False, "message": "O CEP deve conter apenas números e ser composto por 8 caracteres."}, 400
     
     #validar número
-    if not number.isdigit() or len(number) > 20:
+    if not (data.get("Numero")or"").isdigit() or len((data.get("Numero")or"")) > 20:
         return {"success": False, "message": "O número deve conter apenas números e ser menor que 20 caracteres."}, 400
     
     #validar cidade
-    if not city.replace(" ","").isalpha() or len(city) > 100:
+    if not (data.get("Cidade")or"").replace(" ","").isalpha() or len((data.get("Cidade")or"")) > 100:
         return {"success": False, "message": "A cidade deve conter apenas letras e ser menor que 100 caracteres."}, 400
     
     #validar estado
-    if not state.replace(" ","").isalpha() or len(state) > 100:
+    if not (data.get("Estado")or"").replace(" ","").isalpha() or len((data.get("Estado")or"")) > 100:
         return {"success": False, "message": "O estado deve conter apenas letras e ser menor que 100 caracteres."}, 400
     
     #validar país
-    if not country.replace(" ","").isalpha() or len(country) > 50:
+    if not (data.get("Pais")or"").replace(" ","").isalpha() or len((data.get("Pais")or"")) > 50:
         return {"success": False, "message": "O país deve conter apenas letras e ser menor que 50 caracteres."}, 400
     
     #validar complemento
-    # mudar a lógica para permitir que a data de sáida seja none
-    if complement and (not complement.replace(" ","").isalnum() or len(complement) > 100):
+    if (data.get("Complemento")or"") and (not (data.get("Complemento")or"").replace(" ","").isalnum() or len((data.get("Complemento")or"")) > 100):
         return {"success": False, "message": "O complemento deve conter apenas letras e números e ser menor que 100 caracteres."}, 400
-    
-    #validar setor
-    if not sector in [UserSector.MARKETING.name, UserSector.RH.name, UserSector.CONTENT.name, UserSector.INSTRUCTORS.name]:
-        return {"success": False, "message": "Setor inválido."}, 400
-    
-    #vaidar posição
-    if not position in [UserPosition.STUDANT_COORDINATOR.name, UserPosition.REPRESENTATIVE.name, UserPosition.VOLUNTEER.name]:
-        return {"success": False, "message": "Posição inválida."}, 400
-    
-    #validar tipo de usuário
-    if not (profile in [UserProfile.COORDINATOR.name, UserProfile.SCHOLARSHIP.name, UserProfile.VOLUNTEER.name]):
-        return {"success": False, "message": "Tipo de usuário inválido."}, 400
-    
-    #validar status
-    if not status in [UserStatus.ACTIVE.name, UserStatus.INACTIVE.name, UserStatus.DELETED.name]:
-        return {"success": False, "message": "Status inválido."}, 400
     
     old_user = current_user
     
     try:
         old_user.name = (data.get('Nome')or"").strip()
+        old_user.email = (data.get("Email")or"").strip()
+        old_user.profession = (data.get("Profissao")or"").strip()
+        old_user.marital = (data.get("Estado_Civil")or"").strip()
+        old_user.code_institutional = (data.get("Registro_Academico")or"").strip()
+        old_user.nationality = (data.get("Nacionalidade")or"").strip()
+        old_user.street = (data.get("Logradouro")or"").strip()
+        old_user.neighborhood = (data.get("Bairro")or"").strip()
+        old_user.postal_code = (data.get("CEP")or"").strip()
+        old_user.number = (data.get("Numero")or"").strip()
+        old_user.city = (data.get("Cidade")or"").strip()
+        old_user.state = (data.get("Estado")or"").strip()
+        old_user.country = (data.get("Pais")or"").strip()
+        old_user.complement = (data.get("Complemento")or"").strip()
     
         db.session.commit()
-        return {"success": True, "message": "Usuário Registrado com Sucesso."}, 201
+        return {"success": True, "message": "Dados atualizados com Sucesso."}, 201
     except:
         db.session.rollback()
-        return {"success": False, "message": "Erro ao registrar usuário."}, 500
-''
+        return {"success": False, "message": "Erro ao atualizar dados."}, 500
